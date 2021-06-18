263effce6f4312562f7616002b78bf222a9fc13d
kashsha <kapoorshashanksk@gmail.com>
2021-06-18 15:38:41 +0530

    Shashank: Added Confirm Password functionality

--- Frontend/shoppingCart/src/app/core/auth/register/register.component.ts
+++ Frontend/shoppingCart/src/app/core/auth/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, AbstractControl, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserService} from '../../user.service';
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-register',
@@ -15,35 +15,65 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private userService: UserService
-    // private _fb: FormBuilder
+    private userService: UserService,
   ) {
 
   }
 
   ngOnInit(): void {
+
     this.registrationForm = new FormGroup({
       firstName: new FormControl('', Validators.required),
       lastName: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
-    });
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl('', [Validators.required],)
 
-}
-onSubmit() {
-  
-  const usrData = {
-    name: this.registrationForm.value.firstName, email: this.registrationForm.value.email,
-    password: this.registrationForm.value.password, isActive: true
-  };
-  const registeredUsersData = JSON.parse(localStorage.getItem('userData')) || [];
-  const index = registeredUsersData.findIndex((item) => item.email === usrData.email);
-  if (index === -1) {
-    this.errorMsg = '';
-    this.userService.setDataInLocalStorage(usrData);
-    this.router.navigateByUrl('/product');
-  } else {
-    this.errorMsg = 'User with this emailId already exist';
+
+    }, this.pwdMatchValidator);
+
   }
-}
+  onSubmit() {
+
+    const usrData = {
+      name: this.registrationForm.value.firstName, email: this.registrationForm.value.email,
+      password: this.registrationForm.value.password, isActive: true
+    };
+    const registeredUsersData = JSON.parse(localStorage.getItem('userData')) || [];
+    const index = registeredUsersData.findIndex((item) => item.email === usrData.email);
+    if (index === -1) {
+      this.errorMsg = '';
+      this.userService.setDataInLocalStorage(usrData);
+      this.router.navigateByUrl('/product');
+    } else {
+      this.errorMsg = 'User with this emailId already exist';
+    }
+  }
+
+  pwdMatchValidator(frm: FormGroup) {
+    debugger;
+    return frm.get('password').value === frm.get('confirmPassword').value
+      ? null : { 'mismatch': true };
+  }
+
+  get password() { return this.registrationForm.get('password'); }
+  get confirmPassword() { return this.registrationForm.get('confirmPassword'); }
+  MustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        // return if another validator has already found an error on the matchingControl
+        return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    }
+  }
 }
